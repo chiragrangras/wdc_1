53b32906fc15216228e63da708fc074ad18e62f5
Chirag Rangras <chiragrangras012@gmail.com>
2024-06-02 18:22:53 +0530

    modified file

--- React_project/react-essentials/01-starting-project/src/App.jsx
+++ React_project/react-essentials/01-starting-project/src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/Tabbutton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  let tabContent = 'Please click a Button'
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components','jsx','props','state'
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
   }
 
   return (
@@ -43,7 +46,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
