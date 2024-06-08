a7f199e89198c45d73f83fdd1fc4d986d03da7f0
Chirag Rangras <chiragrangras012@gmail.com>
2024-06-08 11:02:22 +0530

    modified file

--- React_project/react-essentials/03-tic-tac-toe-starting/src/components/Player.jsx
+++ React_project/react-essentials/03-tic-tac-toe-starting/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({name,symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditting] = useState(false);
+
+  function handleEditClick() {
+    setIsEditting((editing)=>!editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+    // btnCaption='Save';
+  }
+
   return (
     <li>
       <span className="player-info">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
