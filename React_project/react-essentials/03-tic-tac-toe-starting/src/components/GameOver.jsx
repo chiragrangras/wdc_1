adca411f883ee6f38e6623fc7e0c082763d1d985
Chirag Rangras <chiragrangras012@gmail.com>
2024-06-11 18:26:57 +0530

    modified file

--- React_project/react-essentials/03-tic-tac-toe-starting/src/components/GameOver.jsx
+++ React_project/react-essentials/03-tic-tac-toe-starting/src/components/GameOver.jsx
@@ -1,11 +1,11 @@
-export default function GameOver({ winner,onRestart }) {
+export default function GameOver({ winner }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
-      {!winner && <p>It's draw!</p>}
+      {!winner && <p>It's a draw!</p>}
       <p>
-        <button onClick={onRestart}>Rematch!</button>
+        <button>Rematch!</button>
       </p>
     </div>
   );
