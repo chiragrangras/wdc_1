238f04f0dc7df79b2b27d90fbf8c14048fdaf13f
Chirag Rangras <chiragrangras012@gmail.com>
2024-08-20 16:14:08 +0530

    modified file

--- Omnifood-Optimizations/js/script.js
+++ Omnifood-Optimizations/js/script.js
@@ -5,13 +5,15 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-h1.addEventListener("click",function(){
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
-});
+// h1.addEventListener("click",function(){
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
 
 const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
