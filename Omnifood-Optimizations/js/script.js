8ec58c5cda15371f0082b5af230faadcb71216f8
Chirag Rangras <chiragrangras012@gmail.com>
2024-08-21 16:48:11 +0530

    modified file

--- Omnifood-Optimizations/js/script.js
+++ Omnifood-Optimizations/js/script.js
@@ -31,7 +31,21 @@ btnNavEL.addEventListener("click", function () {
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
-console.log(allLinks);
+allLinks.forEach(function(link){
+  link.addEventListener("click",function(e){
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if(href === "#"){
+      window.scrollTo({
+        top:0,
+        behavior:"smooth",
+      });
+    }
+  })
+})
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
