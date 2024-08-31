273c99f860ff481f511c6e837b4516d1499428fe
Chirag Rangras <chiragrangras012@gmail.com>
2024-08-31 17:22:29 +0530

    modified file

--- Javascript/01-Fundamentals-Part-1/script.js
+++ Javascript/01-Fundamentals-Part-1/script.js
@@ -45,7 +45,6 @@ year = 1993;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 // let age = 30;
 // age = 31;
@@ -63,7 +62,7 @@ console.log(typeof null);
 // Math operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2019;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -84,3 +83,21 @@ console.log(x);
 // Comparison operators
 console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah,averageAge);
