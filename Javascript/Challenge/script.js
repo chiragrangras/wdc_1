7fda838248cdae64b021e48740292959689ec9f4
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-01 16:32:28 +0530

    modified file

--- Javascript/Challenge/script.js
+++ Javascript/Challenge/script.js
@@ -2,9 +2,9 @@ const massMark = 78;
 const heightMark = 1.69; // meters
 const massJohn = 92;
 const heightJohn = 1.95;
- 
-const BMIMark = massMark / (heightMark * heightMark);
+
+const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
- 
+
 const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn);
