745506ebe8ba5f86a681cd91f844273cbe942960
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-02 17:56:17 +0530

    modified file

--- Javascript/01-Fundamentals-Part-1/script.js
+++ Javascript/01-Fundamentals-Part-1/script.js
@@ -161,7 +161,6 @@ console.log("23" / "2");
 let n = "1" + 1; // "11"
 n = n - 1;
 console.log(n);
-*/
 
 // 5 falsy values: 0, '',undefined,null,NaN
 
@@ -184,3 +183,24 @@ if (height) {
 } else {
   console.log("Height is UNDEFINED");
 }
+*/
+
+const age = "18";
+if (age === 18) console.log("You just became an adult! (strict)");
+if (age == 18) console.log("You just became an adult! (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite == 23) {
+  console.log("Cool! 23 is an amzaing number!");
+} else if (favourite === 7) {
+  console.log("7 also a cool number!");
+} else if (favourite === 9) {
+  console.log("9 also a cool number!");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
