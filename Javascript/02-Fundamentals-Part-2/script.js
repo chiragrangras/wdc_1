5740aab8150bd7bf2ae46b56b58e0044a0199f5c
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-08 18:23:50 +0530

    modified file

--- Javascript/02-Fundamentals-Part-2/script.js
+++ Javascript/02-Fundamentals-Part-2/script.js
@@ -1,10 +1,27 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log('I can driver');
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log('I can driver');
 
 // const interface = 'Audio';
 // const private = 534;
+
+function logger(){
+    console.log('My name is Joans.');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor (apples, oranges) {
+    console.log(apples,oranges);
+    const juise = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juise;
+}
+
+fruitProcessor(5,0);
