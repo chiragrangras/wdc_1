9961d1c2f16f89b2b8e111d93178d533281fbb45
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-10 18:25:13 +0530

    modified file

--- Javascript/02-Fundamentals-Part-2/script.js
+++ Javascript/02-Fundamentals-Part-2/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // let hasDriversLicense = false;
 // const passTest = true;
@@ -9,7 +9,7 @@
 // const interface = 'Audio';
 // const private = 534;
 
-function logger(){
+/* function logger(){
     console.log('My name is Joans.');
 }
 
@@ -30,3 +30,35 @@ const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+// function declarartion 
+function calcAge1 (birthYear){
+    return 2024 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// function expression
+const calcAge2 = function (birthYear){
+    return 2024 - birthYear;
+}
+
+const age2 = calcAge2(1993);
+
+console.log(age1,age2);
+*/
+
+// Arrow function
+const calcAge3 = (birthYear) => 2024 - birthYear;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+const yeasrUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yeasrUntilRetirement(1993, 'Jonas'));
+console.log(yeasrUntilRetirement(1980, 'Bob'));
