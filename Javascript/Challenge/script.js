c8080cfa2dd1c8b57005268fee25a69454d1cc8d
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-14 16:34:33 +0530

    modified file

--- Javascript/Challenge/script.js
+++ Javascript/Challenge/script.js
@@ -61,7 +61,6 @@ const bill = 275;
 
 const tip = bill >=50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the trip was ${tip}, and the total value ${bill + tip}`);
-*/
 
 // Test 1
 const calcAverage = (a, b, c) => (a + b + c) / 3;
@@ -78,7 +77,7 @@ const checkWinner = function (avgDolphins, avgKoalas) {
     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
     console.log("No team wins...");
-  }
+}
 };
 
 checkWinner(scoreDolphins, scoreKoalas);
@@ -89,3 +88,13 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
+*/
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+ 
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+ 
+console.log(bills, tips, totals);
