08d32bdfd4290d49fcf3e1110ed50b3dd066838e
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-22 16:46:54 +0530

    modified file

--- Javascript/02-Fundamentals-Part-2/script.js
+++ Javascript/02-Fundamentals-Part-2/script.js
@@ -374,3 +374,13 @@ for (exercise = 1; exercise <= 3; exercise++) {
   }
 }
 */
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weights repetition ${rep}`);
+  rep++;
+}
