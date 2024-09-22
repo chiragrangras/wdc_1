6d1815b9a081fd848e3590173169434cd4ddc0dc
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-22 17:32:24 +0530

    modified file

--- Javascript/Challenge/script.js
+++ Javascript/Challenge/script.js
@@ -133,8 +133,7 @@ if (mark.bmi > john.bmi) {
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
- 
- 
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
