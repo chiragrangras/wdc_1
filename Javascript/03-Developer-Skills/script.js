5494342d7b46ad86bddfebea6762a1f3896db4f8
Chirag Rangras <chiragrangras012@gmail.com>
2024-09-25 18:06:44 +0530

    modified file

--- Javascript/03-Developer-Skills/script.js
+++ Javascript/03-Developer-Skills/script.js
@@ -74,7 +74,8 @@ const measureKelvin = function () {
     unit: 'celsius',
 
     // C) FIX
-    value: Number(prompt('Degrees celsius:')),
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
   };
 
   // B) FIND
@@ -89,3 +90,29 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  let i;
+
+  for (i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
